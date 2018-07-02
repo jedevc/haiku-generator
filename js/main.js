ba9f94dbbd77c163d361f0ad7754c50762dd2f96
Justin Chadwell <jedevc@gmail.com>
2018-07-02 16:13:26 +0100

    Add basic web page generation

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const txt = require('../samples/nouns.txt')
+
+window.onload = () => {
+  console.log(txt)
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'js/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt/,
+        use: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      inject: 'body'
+    })
+  ],
+  mode: 'development'
+}
