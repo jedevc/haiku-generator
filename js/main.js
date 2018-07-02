acfe849c98c618164cb21d0726817d8f2b1431a6
Justin Chadwell <jedevc@gmail.com>
2018-07-02 16:16:22 +0100

    Add compronise NLP package

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const txt = require('../samples/nouns.txt')
 
+const nlp = require('compromise')
+
 window.onload = () => {
   console.log(txt)
+
+  let str = nlp('dinosaur').nouns().toPlural().out('text')
+  console.log(str)
 }
