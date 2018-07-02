0c8aa7bb0e008eaaec4c357a2657239ec852d2d3
Justin Chadwell <jedevc@gmail.com>
2018-07-02 16:25:01 +0100

    Randomly select words from files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-const txt = require('../samples/nouns.txt')
+const nouns = require('../words/nouns.txt').split('\n')
 
 const nlp = require('compromise')
 
 window.onload = () => {
-  console.log(txt)
+  for (let i = 0; i < 10; i++) {
+    console.log(randomWord(nouns))
+  }
+}
 
-  let str = nlp('dinosaur').nouns().toPlural().out('text')
-  console.log(str)
+function randomChoice(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+function randomWord(array, repeat=true) {
+  while (repeat) {
+    const choice = randomChoice(array)
+    const parts = choice.split(' ')
+    if (parts.length == 2) {
+      return {
+        word: parts[0],
+        syllables: parts[1]
+      }
+    }
+  }
+
+  return null
 }
