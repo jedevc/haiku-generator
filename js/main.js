5a95327857537ce5b1ea087ece8d25c31f9a9c84
Justin Chadwell <jedevc@gmail.com>
2018-07-02 20:07:41 +0100

    Add basic haiku generation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,66 @@
-const nouns = require('../words/nouns.txt').split('\n')
+const noun = require('../words/nouns.txt').split('\n')
+const adjective = require('../words/adjectives.txt').split('\n')
 
 const nlp = require('compromise')
 
 window.onload = () => {
-  for (let i = 0; i < 10; i++) {
-    console.log(randomWord(nouns))
+  let haiku = new Haiku()
+  for (let i = 0; i < 20; i++) {
+    console.log(haiku.generate())
+  }
+}
+
+class Haiku {
+  constructor() {
+  }
+
+  generate() {
+    return [
+      this.part(5),
+      this.part(7),
+      this.part(5)
+    ].join('\n')
+  }
+
+  part(syllables) {
+    let ln
+    do {
+      ln = this.line()
+    } while (ln.syllables != syllables)
+    return ln.word
+  }
+
+  line() {
+    return this.noun()
+  }
+
+  noun() {
+    let base = randomWord(noun)
+    if (Math.random() < 0.4) {
+      base = this.adjective().add(base)
+    }
+
+    const n = Math.random()
+    if (n < 0.25) {
+      return new Word('the').add(base)
+    } else if (n < 0.5) {
+      try {
+        const article = nlp(base.word).nouns().articles()[0].article
+        return new Word(article).add(base)
+      } catch (e) {
+        if (e instanceof TypeError) {
+          return new Word('the').add(base)
+        } else {
+          throw e
+        }
+      }
+    } else {
+      return base
+    }
+  }
+
+  adjective() {
+    return randomWord(adjective)
   }
 }
 
@@ -18,12 +74,29 @@ function randomWord(array, repeat=true) {
     const choice = randomChoice(array)
     const parts = choice.split(' ')
     if (parts.length == 2) {
-      return {
-        word: parts[0],
-        syllables: parts[1]
-      }
+      return new Word(parts[0], Number(parts[1]))
     }
   }
 
   return null
+}
+
+class Word {
+  constructor(word, syllables=null) {
+    this.word = word
+
+    if (syllables == null) {
+      // simple cheat syllable count
+      this.syllables = word.split(/[ aeiou]/)
+                           .filter(s => s.length != 0)
+                           .length
+    } else {
+      this.syllables = syllables
+    }
+  }
+
+  add(other) {
+    return new Word(this.word + ' ' + other.word,
+                    this.syllables + other.syllables)
+  }
 }
